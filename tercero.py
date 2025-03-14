531717160dda7bb8f7496c8e1eaa470801e504fe
MELIM708 <isabel.mr@ucad.edu.mx>
2025-03-14 09:51:34 -0600

    mi primer commit

---
+++ tercero.py
@@ -0,0 +1,6 @@
+print("Los nuemros mayores a 5 de la lista son:")
+print(5,2,6,7,8,10,77,55,2,1,30,4,2,3)
+tupla = (5,2,6,7,8,10,77,55,2,1,30,4,2,3)
+for x in range(0, len(tupla)):
+  if x > 5:
+    print(x)
